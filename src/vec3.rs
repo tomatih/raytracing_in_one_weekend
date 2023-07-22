e8be183b49528c7f232b25513849103983f663a7
to_matih <maatteeoh@wp.pl>
2023-07-22 15:46:14 +0100

    3.2 vec3 Utility Functions

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,32 +1,46 @@
 use std::ops;
 
-#[derive(Debug, PartialEq)]
-struct Vec3{
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct Vec3{
     e: [f32; 3],
 }
 
 // aliases
-type Point3 = Vec3;
-type Color = Vec3;
+pub type Point3 = Vec3;
+pub type Color = Vec3;
 
 impl Vec3 {
     // constructors
-    fn new() -> Self{
+    pub fn new() -> Self{
         Vec3 { e: [0.0, 0.0, 0.0] }
     }
-    fn from_values(x:f32, y:f32, z:f32) -> Self{
+    pub fn from_values(x:f32, y:f32, z:f32) -> Self{
         Vec3 { e: [x, y, z] }
     }
     // accessors
-    fn x(self) -> f32 { self.e[0] }
-    fn y(self) -> f32 { self.e[1] }
-    fn z(self) -> f32 { self.e[2] }
+    pub fn x(self) -> f32 { self.e[0] }
+    pub fn y(self) -> f32 { self.e[1] }
+    pub fn z(self) -> f32 { self.e[2] }
     // lenght
-    fn length_squared(self) -> f32{
+    fn length_squared(&self) -> f32{
         self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
     }
-    fn length(self) -> f32{
+    pub fn length(&self) -> f32{
         f32::sqrt(self.length_squared())
+    }
+    // operations
+    pub fn dot(a: Vec3, b: Vec3) -> f32{
+        a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+    }
+    pub fn cross(a: Vec3, b:Vec3) -> Vec3{
+        Vec3 { e: [
+            a[1]*b[2] - a[2]*b[1],
+            a[2]*b[0] - a[0]*b[2],
+            a[0]*b[1] - a[1]*b[0],
+        ] }
+    }
+    pub fn unit_vector(v: Vec3) -> Vec3{
+        v / v.length()
     }
 }
 
@@ -78,6 +92,61 @@ impl ops::MulAssign<f32> for Vec3 {
 impl ops::DivAssign<f32> for Vec3 {
     fn div_assign(&mut self, rhs: f32) {
         (*self) *= 1.0/rhs;
+    }
+}
+
+impl ops::Add for Vec3{
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3{ e:[
+            self[0] + rhs[0],
+            self[1] + rhs[1],
+            self[2] + rhs[2],
+        ]}
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+       self + -rhs
+    }
+}
+
+impl ops::Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Vec3 {e:[
+            rhs * self.e[0],
+            rhs * self.e[1],
+            rhs * self.e[2],
+        ] }
+    }
+}
+
+impl ops::Mul<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl ops::Div<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        self * (1.0/rhs)
+    }
+}
+
+// display
+impl std::fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} {} {})", self.e[0], self.e[1], self.e[2])
     }
 }
 
@@ -153,14 +222,56 @@ mod tests{
     }
 
     #[test]
-    fn test_length(){
+    fn length(){
         let vec = Vec3::from_values(1.0, 1.0, 1.0);
         assert_eq!(vec.length(), f32::sqrt(3.0));
     }
     #[test]
-    fn test_length_squared(){
+    fn length_squared(){
         let vec = Vec3::from_values(1.0, 1.0, 1.0);
         assert_eq!(vec.length_squared(), 3.0);
     }
-    
+
+    #[test]
+    fn add(){
+        assert_eq!(
+            Vec3::from_values(1.0, 2.0, 3.0) + Vec3::from_values(1.0, 2.0, 3.0),
+            Vec3::from_values(2.0, 4.0, 6.0)
+        )
+    }
+    #[test]
+    fn subtract(){
+        assert_eq!(
+            Vec3::from_values(1.0, 2.0, 3.0) - Vec3::from_values(1.0, 2.0, 3.0),
+            Vec3::new()
+        )
+    }
+    #[test]
+    fn multiply(){
+        assert_eq!(Vec3::from_values(1.0, 2.0, 3.0) * 2.0, Vec3::from_values(2.0, 4.0, 6.0));
+        assert_eq!(2.0 * Vec3::from_values(1.0, 2.0, 3.0) , Vec3::from_values(2.0, 4.0, 6.0));
+    }
+    #[test]
+    fn divide(){
+        assert_eq!(Vec3::from_values(2.0, 4.0, 6.0) / 2.0, Vec3::from_values(1.0, 2.0, 3.0));
+    }
+
+    #[test]
+    fn dot(){
+        let vec = Vec3::from_values(1.0, 2.0, 3.0);
+        assert_eq!(Vec3::dot(vec, vec), 14.0);
+    }
+
+    #[test]
+    fn cross(){
+        let vec = Vec3::from_values(1.0, 2.0, 3.0);
+        assert_eq!(Vec3::cross(vec, vec), Vec3::new());
+    }
+    #[test]
+    fn unit_vector(){
+        let vec = Vec3::from_values(1.0, 2.0, 3.0);
+        let vec_norm = Vec3::unit_vector(vec);
+        assert_eq!(vec_norm.length(), 1.0)
+    }
+
 }
