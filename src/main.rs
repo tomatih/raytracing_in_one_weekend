e73ca425b0dd03ad0453db1dd85cdace32b3791e
to_matih <maatteeoh@wp.pl>
2023-07-22 13:20:56 +0100

    Upgrading to png

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
+use image::{RgbImage, ImageBuffer};
+
 fn main() {
     // Image constants
-    const IMAGE_HEIGHT: i32 = 256;
-    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+    const IMAGE_WIDTH: u32 = 256;
 
-    // Print image header
-    println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
-    println!("255");
+    // Allocate image buffer
+    let mut img: RgbImage = ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     // Render image
-    for j in (0..IMAGE_HEIGHT).rev() {
+    for j in 1..=IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH{
             // normalised values
             let r = i as f32 / (IMAGE_WIDTH-1) as f32;
             let g = j as f32 / (IMAGE_HEIGHT-1) as f32;
             let b = 0.25;
             // pixel values
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
             // print pixel
-            println!("{} {} {}", ir, ig, ib);
+            img.put_pixel(i, IMAGE_HEIGHT-j, image::Rgb([ir, ig, ib]))
         }
     }
+    // save image
+    img.save("out.png").expect("Faild to save image");
 }
