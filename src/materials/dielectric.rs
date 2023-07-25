6a4d4bba8a0a813a7005ecd8cf1a98bbc3d580d8
to_matih <maatteeoh@wp.pl>
2023-07-25 20:53:14 +0100

    10.4 Schlick Approximation

--- src/materials/dielectric.rs
+++ src/materials/dielectric.rs
@@ -1,4 +1,5 @@
 use cgmath::InnerSpace;
+use rand::Rng;
 
 use crate::{ray::Ray, hit_system::HitRecord, common::{Color, refract, reflect}};
 
@@ -22,7 +23,9 @@ impl Material for Dielectric {
         let cos_theta = hit_record.normal.dot(-unit_direction).min(1.0);
         let sin_theta = (1.0 - cos_theta*cos_theta).sqrt();
 
-        let direction = if refraction_ratio* sin_theta > 1.0{
+        let canot_refrect = refraction_ratio* sin_theta > 1.0;
+        let mut rng = rand::thread_rng();
+        let direction = if canot_refrect || reflectance(cos_theta, refraction_ratio) > rng.gen::<f32>(){
             reflect(unit_direction, hit_record.normal)
         } else {
             refract(unit_direction, hit_record.normal, refraction_ratio)
@@ -30,4 +33,10 @@ impl Material for Dielectric {
         
         Some((Color::new(1.0, 1.0, 1.0), Ray{ origin: hit_record.p, direction}))
     }
+}
+
+fn reflectance(cosine: f32, ref_idx: f32) -> f32{
+    let r0 = (1.0-ref_idx) / (1.0+ref_idx);
+    let r0 = r0*r0;
+    r0 + (1.0-r0)*(1.0- cosine).powi(5)
 }
